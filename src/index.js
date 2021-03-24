10c656762eeea97fe2deac1e12a8fa7cd8905e4a
Eva <eva.thorisdottir@familjen.se>
2021-03-24 13:59:23 +0100

    Initial setup with boilerplate redux

--- src/index.js
+++ src/index.js
@@ -2,16 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+// REDUX
+import { Provider } from 'react-redux';
+import store from './redux/store.js';
+
+import CssBaseline from '@material-ui/core/CssBaseline'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  <Provider store={store}>
+    <React.StrictMode>
+      <CssBaseline />
+      <App />
+    </React.StrictMode>
+  </Provider>,
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  document.getElementById('root'),
+
+);
