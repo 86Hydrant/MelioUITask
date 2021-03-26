26552094d3ce5a74a8757f304860c0360537271c
Eva <eva.thorisdottir@familjen.se>
2021-03-26 12:44:25 +0100

    fixes to cart item styling, added helper function to shorten name of item in cart

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -12,39 +12,63 @@ import { removeFromCart, addToCart, } from "../redux/actions/cartActions"
 const useStyles = makeStyles({
     cartContainer: {
         padding: "20px",
+        minWidth: "350px"
     },
-    cartItem: {
+    cartItemContainer: {
         listStyle: "none",
+        margin: "20px 0px"
+
+
     },
     quantityButtonContainer: {
         border: "1px solid lightgray",
         borderRadius: "5px",
-        padding: "5px 1px",
-        marginLeft: "10px",
+        marginLeft: "auto",
     },
     itemCount: {
         borderRight: "1px solid lightgray",
         borderLeft: "1px solid lightgray",
         padding: "5px 10px"
     },
+    cartItem: {
+        width: "100%",
+        display: "flex",
+        justifyContent: "space-between",
+    },
+    buttons: {
+        padding: "1px 4px",
+    }
 });
 
 function ShoppingCart() {
     let cartItems = useSelector(state => state.cart.cartItems);
-
     const dispatch = useDispatch();
 
     const classes = useStyles();
 
+    const limitNameLength = (name) => {
+        if (name.length > 19) {
+            return `${name.substr(0, 19)}...`
+        } else {
+            return name
+        }
 
+    }
 
     return (
         <Paper variant="elevation" className="cart-container">
             <Grid container className={classes.cartContainer}>
                 <ul>
                     {cartItems && cartItems.map(item => (
-                        <li className={classes.cartItem}>{item.name}
-                            <span className={classes.quantityButtonContainer}><IconButton onClick={() => dispatch(removeFromCart(item))} size="small"><RemoveIcon /></IconButton><span className={classes.itemCount}>{item.count}</span><IconButton onClick={() => dispatch(addToCart(item))} size="small"><AddIcon /></IconButton></span>
+                        <li className={classes.cartItemContainer}>
+                            <Grid item direction="row" className={classes.cartItem}>
+                                {limitNameLength(item.name)}
+                                <span className={classes.quantityButtonContainer}>
+                                    <IconButton onClick={() => dispatch(removeFromCart(item))} className={classes.buttons}><RemoveIcon /></IconButton>
+                                    <span className={classes.itemCount}>{item.count}</span>
+                                    <IconButton onClick={() => dispatch(addToCart(item))} className={classes.buttons}><AddIcon /></IconButton>
+                                </span>
+                            </Grid>
                         </li>
                     ))
                     }
